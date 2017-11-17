ea0102ff038159ce0eaffe9ba8eba6493b3fd207
Christopher Klumb <christopher.klumb@hotmail.de>
2017-11-17 16:30:31 +0100

    Kommentre Entfernt

--- WebSiteFinal/HumidityPusher.cs
+++ WebSiteFinal/HumidityPusher.cs
@@ -20,10 +20,7 @@ namespace WebSiteFinal
         public static HumidityPusher Instance { get { return _instance.Value; } }
 
         private IHubConnectionContext<dynamic> Clients { get; set; }
-
-        // nur zum testen
-        //private readonly ConcurrentDictionary<string, HumidityInfo> _humidityInfos = new ConcurrentDictionary<string, HumidityInfo>();
-
+        
         static string connectionString = "HostName=VtIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Hc7T95hsBsesDH/SaRBXJa2yNYzLc7qa4ptoq3q1aPE=";
 
         static EventHubClient eventHubClient;
@@ -31,27 +28,14 @@ namespace WebSiteFinal
         private HumidityPusher(IHubConnectionContext<dynamic> clients)
         {
             Clients = clients;
-
-            /*
-            _humidityInfos.Clear();
-            var stocks = new List<HumidityInfo>
-            {
-                new HumidityInfo { Device = "GOOG", Humidity = "570.30" }
-            };
-            stocks.ForEach(info => _humidityInfos.TryAdd(info.Device, info));
-            */
-
-            // TODO: hier IoTHubListener implementieren
+            
             eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, "messages/events");
             var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
-
-            List<Task> tasks = new List<Task>();
+            
             foreach (string partition in d2cPartitions)
             {
-                //tasks.Add(ReceiveMessagesFromDeviceAsync(partition));
                 Task.Run(() => ReceiveMessagesFromDeviceAsync(partition));
             }
-            //Task.WhenAll(tasks.ToArray());
         }
 
         private async Task ReceiveMessagesFromDeviceAsync(string partition)
@@ -70,12 +54,5 @@ namespace WebSiteFinal
                 } catch { }
             }
         }
-
-        /*
-        public HumidityInfo GetAllHumidityInfos()
-        {
-            return _humidityInfos["GOOG"];
-        }
-        */
     }
 }
