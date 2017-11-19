21b8e9bda87171e91dd7eba48ce970d57581e7d1
Christopher Klumb <christopher.klumb@hotmail.de>
2017-11-19 15:27:46 +0100

    WebJob aufgeräumt und Abhängigkeiten ergänzt

---
+++ WebJob/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace WebJob
+{
+    class Program
+    {
+        static string connectionString = "HostName=VtIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Hc7T95hsBsesDH/SaRBXJa2yNYzLc7qa4ptoq3q1aPE=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+            
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition));
+            }
+            Task.WaitAll(tasks.ToArray());
+
+        }
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow.AddHours(-200));
+            while (true)
+            {
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+    }
+}
